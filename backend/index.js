f8265e603e0a75e34c02f4a546ad43960e9605b8
DocDuck <alex.mitin.psmu@gmail.com>
2023-05-27 00:18:06 +0500

    init

---
+++ backend/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const redis = require('redis');
+const { Pool } = require('pg');
+
+const app = express();
+
+// Redis client setup
+const redisClient = redis.createClient({
+  host: 'redis', // Name of the Redis service defined in the Docker Compose file
+});
+
+redisClient.on('connect', () => {
+  console.log('Connected to Redis');
+});
+
+redisClient.on('error', (err) => {
+  console.error('Error connecting to Redis:', err);
+});
+
+// PostgreSQL connection pool setup
+const pool = new Pool({
+  user: 'app', // PostgreSQL username defined in the Docker Compose file
+  password: 'postgres', // PostgreSQL password defined in the Docker Compose file
+  host: 'postgres', // Name of the PostgreSQL service defined in the Docker Compose file
+  database: 'postgres', // PostgreSQL database name defined in the Docker Compose file
+  port: 5432, // PostgreSQL default port
+});
+
+pool.on('connect', () => {
+  console.log('Connected to PostgreSQL');
+});
+
+pool.on('error', (err) => {
+  console.error('Error connecting to PostgreSQL:', err);
+});
+
+// Sample API endpoint
+app.get('/', (req, res) => {
+  // Perform Redis and PostgreSQL operations here
+  redisClient.set('key', 'value', (redisErr) => {
+    if (redisErr) {
+      console.error('Error setting key in Redis:', redisErr);
+    }
+
+    pool.query('SELECT NOW()', (pgErr, pgRes) => {
+      if (pgErr) {
+        console.error('Error executing PostgreSQL query:', pgErr);
+        res.status(500).json({ error: 'Internal server error' });
+      } else {
+        const currentTime = pgRes.rows[0].now;
+        res.json({ redisValue: 'value', currentTime });
+      }
+    });
+  });
+});
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
