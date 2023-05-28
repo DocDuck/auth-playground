4c403132cc988d24f5464cfd369a9e08a015f762
DocDuck <alex.mitin.psmu@gmail.com>
2023-05-28 12:36:10 +0500

    wip

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,29 @@
-const express = require('express');
-const redis = require('redis');
-const { Pool } = require('pg');
+import dotenv  from "dotenv"
+import express from 'express';
+// import redis from 'redis';
+import pg from 'pg';
+dotenv.config()
+
+const { Pool } = pg;
 
 const app = express();
 
 // Redis client setup
-const redisClient = redis.createClient({
-  host: 'redis', // Name of the Redis service defined in the Docker Compose file
-});
+// const redisClient = redis.createClient({
+//   host: 'redis', // Name of the Redis service defined in the Docker Compose file
+// });
 
-redisClient.on('connect', () => {
-  console.log('Connected to Redis');
-});
+// redisClient.on('connect', () => {
+//   console.log('Connected to Redis');
+// });
 
-redisClient.on('error', (err) => {
-  console.error('Error connecting to Redis:', err);
-});
+// redisClient.on('error', (err) => {
+//   console.error('Error connecting to Redis:', err);
+// });
 
 // PostgreSQL connection pool setup
 const pool = new Pool({
-  user: 'app', // PostgreSQL username defined in the Docker Compose file
+  user: 'postgres', // PostgreSQL username defined in the Docker Compose file
   password: 'postgres', // PostgreSQL password defined in the Docker Compose file
   host: 'postgres', // Name of the PostgreSQL service defined in the Docker Compose file
   database: 'postgres', // PostgreSQL database name defined in the Docker Compose file
@@ -37,24 +41,24 @@ pool.on('error', (err) => {
 // Sample API endpoint
 app.get('/', (req, res) => {
   // Perform Redis and PostgreSQL operations here
-  redisClient.set('key', 'value', (redisErr) => {
-    if (redisErr) {
-      console.error('Error setting key in Redis:', redisErr);
-    }
+  // redisClient.set('key', 'value', (redisErr) => {
+  //   if (redisErr) {
+  //     console.error('Error setting key in Redis:', redisErr);
+  //   }
 
-    pool.query('SELECT NOW()', (pgErr, pgRes) => {
-      if (pgErr) {
-        console.error('Error executing PostgreSQL query:', pgErr);
-        res.status(500).json({ error: 'Internal server error' });
-      } else {
-        const currentTime = pgRes.rows[0].now;
-        res.json({ redisValue: 'value', currentTime });
-      }
-    });
+
+  // });
+  pool.query('SELECT NOW()', (pgErr, pgRes) => {
+    if (pgErr) {
+      console.error('Error executing PostgreSQL query:', pgErr);
+      res.status(500).json({ error: 'Internal server error' });
+    } else {
+      const currentTime = pgRes.rows[0].now;
+      res.json({ redisValue: 'value', currentTime });
+    }
   });
 });
 
-const port = 5000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
---
+++ clear-and-run.sh
@@ -0,0 +1,3 @@
+docker system prune
+docker image prune -a
+docker compose up --build
