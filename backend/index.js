60632600250150a6de90f6e0a70782304a877180
DocDuck <alex.mitin.psmu@gmail.com>
2023-05-30 09:36:31 +0500

    setup docker done

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,42 @@
 import dotenv  from "dotenv"
-import express from 'express';
-// import redis from 'redis';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import redis from 'redis';
 import pg from 'pg';
 dotenv.config()
+
+const PORT = process.env.PORT;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_USER = process.env.DB_USER;
+const DB_NAME = process.env.DB_NAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 
 const { Pool } = pg;
 
 const app = express();
 
 // Redis client setup
-// const redisClient = redis.createClient({
-//   host: 'redis', // Name of the Redis service defined in the Docker Compose file
-// });
+const redisClient = redis.createClient({
+  host: 'redis', // Name of the Redis service defined in the Docker Compose file
+});
 
-// redisClient.on('connect', () => {
-//   console.log('Connected to Redis');
-// });
+redisClient.on('connect', () => {
+  console.log('Connected to Redis');
+});
 
-// redisClient.on('error', (err) => {
-//   console.error('Error connecting to Redis:', err);
-// });
+redisClient.on('error', (err) => {
+  console.error('Error connecting to Redis:', err);
+});
 
 // PostgreSQL connection pool setup
 const pool = new Pool({
-  user: 'postgres', // PostgreSQL username defined in the Docker Compose file
-  password: 'postgres', // PostgreSQL password defined in the Docker Compose file
-  host: 'postgres', // Name of the PostgreSQL service defined in the Docker Compose file
-  database: 'postgres', // PostgreSQL database name defined in the Docker Compose file
-  port: 5432, // PostgreSQL default port
+  host: DB_HOST, // Name of the PostgreSQL service defined in the Docker Compose file
+  port: DB_PORT, // PostgreSQL default port
+  database: DB_NAME, // PostgreSQL database name defined in the Docker Compose file
+  user: DB_USER, // PostgreSQL username defined in the Docker Compose file
+  password: DB_PASSWORD, // PostgreSQL password defined in the Docker Compose file
 });
 
 pool.on('connect', () => {
@@ -45,8 +54,6 @@ app.get('/', (req, res) => {
   //   if (redisErr) {
   //     console.error('Error setting key in Redis:', redisErr);
   //   }
-
-
   // });
   pool.query('SELECT NOW()', (pgErr, pgRes) => {
     if (pgErr) {
@@ -58,7 +65,19 @@ app.get('/', (req, res) => {
     }
   });
 });
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+app.get('/download', (req, res) => {
+  res.download('/test.pdf', '/test2.pdf');
 });
+
+
+const run = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (e) {
+
+  }
+};
+
+run();
